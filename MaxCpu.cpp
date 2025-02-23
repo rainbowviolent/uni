1055e70ce996c42a42bb2d51cd732ee4da3a8ac2
rainbowviolent <david.fightet4@gmail.com>
2025-02-23 18:14:35 +0000

    Update MaxCpu.cpp

--- MaxCpu.cpp
+++ MaxCpu.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <thread>
+using namespace std;
 
+void workerFunction() {
+    while (true) {
+        
+    }
+}
+
+int main() {
+    const int numThreads = thread::hardware_concurrency();
+
+    for (int i = 0; i < numThreads; ++i) {
+        thread(workerFunction).detach();
+    }
+
+    this_thread::sleep_for(chrono::seconds(10));
+
+    return 0;
+}
